071259befff60258acbecd7e9468c2081ad1c164
devin.desktop <devinobowen@gmail.com>
2019-07-14 01:10:49 -0400

    Updates

---
+++ precache-manifest.76b23507e2a69b4650960383c8565fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d88a3de837b406d6dec248cbcfdcb2",
+    "url": "/GymWebsite/index.html"
+  },
+  {
+    "revision": "69cafc6b6dd64adfbb58",
+    "url": "/GymWebsite/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/GymWebsite/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "69cafc6b6dd64adfbb58",
+    "url": "/GymWebsite/static/js/main.e18911b1.chunk.js"
+  },
+  {
+    "revision": "23996032d06cf9e4d45e",
+    "url": "/GymWebsite/static/js/runtime~main.6c2a1c77.js"
+  }
+]);
